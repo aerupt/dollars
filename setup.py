0bd9ca1e98096da24d83c822cf381cee37ae87ab
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-02-21 12:37:33 +0100

    README: Convert to rst

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ from setuptools import find_packages, setup
 with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
 
 setup(
     name='dollars',
     version='0.1',
     description='Converts everything to dollars using up to date rates for '
                 'free',
+    long_description=long_description,
     url='http://github.com/aerupt/dollars',
     author='Lasse Schuirmann',
     author_email='lasse.schuirmann@gmail.com',
